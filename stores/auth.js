4808fe41076581f258ebf89ea5ab368f8e1ca7c2
Prashanth Mogali <m.prashanth54@gmail.com>
2020-03-30 21:37:18 -0400

    Prashanth | Login, Mobx Code

---
+++ stores/auth.js
@@ -0,0 +1,22 @@
+import { observable, action } from "mobx"
+import axios from '../axios'
+
+class Auth {
+    @observable authToken = ''
+
+    @action
+    async login(email, pass) {
+        try {
+            const resp = await axios.post('/auth/login', { email: email, pass: pass })
+            const { token } = resp.data
+            this.authToken = token
+            return true
+        } catch (err) {
+            //Return invalid credentials
+            return false
+        }
+
+    }
+}
+
+export default new Auth()
