e0326be3d46592a8605944a9d6219d5ab4ac7081
Prashanth Mogali <prashanthmogali@Prashanths-MacBook-Pro.local>
2020-03-30 21:37:18 -0400

    Prashanth | Login, Mobx Code

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View } from 'react-native'
 import { Text, Button } from 'react-native-elements'
+import { observer } from "mobx-react"
+import Auth from '../stores/auth'
 
+@observer
 export default class LoginScreen extends React.Component {
 
     constructor(props) {
@@ -9,9 +12,11 @@ export default class LoginScreen extends React.Component {
         console.log("I'm in Login")
     }
 
-    login() {
+    async login() {
         //Write the implementation to verify and check here. If all is successful call this method below
-        this.props.navigation.navigate('Main')
+        // this.props.navigation.navigate('Main')
+        const isValid = await Auth.login('m.prashanth54@gmail.com', 'Test@123')
+        if (isValid) this.props.navigation.navigate('Main')
     }
 
     render() {
