34554bf926826863adc34823b0de47ee0c8ebd89
Prashanth Mogali <prashanthmogali@Prashanths-MacBook-Pro.local>
2020-04-08 21:19:15 -0400

    Prashanth | Minor bug fixes

--- axios.js
+++ axios.js
@@ -1,11 +1,8 @@
 import * as axios from 'axios'
-import { observer } from 'mobx-react'
-import Auth from './stores/auth'
 
 const instance = axios.create({
     baseURL: 'https://giftslice.herokuapp.com',
-    timeout: 1000,
-    headers: { 'Authorization': '' }
+    timeout: 1000
 });
 
 export default instance
